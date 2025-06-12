245c40ab4d48aa4d375b132cd915bb5159b6b5a2
Obada Talafha <obada.talafha20@gmail.com>
2025-06-12 03:41:49 +0300

    fixing

--- Back-end/app.js
+++ Back-end/app.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 
-// Import routes (auth)
+// Import routess (auth)
 import { loginStudent, loginInstructor } from './routes/auth.js';
 
 // Student routes
